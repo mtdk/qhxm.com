f5c978eb682973a959b3e56be4dbc5781de692d9
xrx1000_001 <xrx1000_001@163.com>
2023-04-18 18:03:11 +0800

    增加分散机跨天关机功能和修改用户权限管理界面

--- data_update/fssb_rds_up.php
+++ data_update/fssb_rds_up.php
@@ -1,13 +1,14 @@
 <?php
 include __DIR__ . '/../user_session/user_session.php';
 include __DIR__ . '/../user_session/userinfo_check.php';
+include __DIR__ . '/../db/db.php';
 include __DIR__ . '/../myHeader.php';
 include __DIR__ . '/../myMenu.php';
 ?>
     <main class="flex-shrink-0">
         <div class="container mt-lg-auto">
             <table class="table caption-top table-hover table-success text-primary table-sm text-center">
-                <caption>未关停分散设备运行记录</caption>
+                <caption><h4><span style="color: black">未关停分散设备</span></h4></caption>
                 <thead>
                 <tr>
                     <th scope="col">#</th>
@@ -26,8 +27,25 @@ include __DIR__ . '/../myMenu.php';
                 <?php
                 $stmt = $dbh->prepare("SELECT id,machine_id,register_date,register_time,pro_id,bath_number,machine_status,uname FROM fssbjl_show where machine_status='开机' order by register_date,register_time");
                 $stmt->execute();
-                $result = $stmt->fetchAll();
-                var_dump($result);
+                $rows = $stmt->fetchAll();
+                $length = count($rows);
+                for ($i = 0; $i < $length; $i++) { ?>
+                    <tr>
+                        <th scope="row"><?php echo $i + 1; ?></th>
+                        <td><?php echo $rows[$i]['id']; ?></td>
+                        <td><?php echo $rows[$i]['machine_id']; ?></td>
+                        <td><?php echo $rows[$i]['register_date']; ?></td>
+                        <td><?php echo substr($rows[$i]['register_time'], 0, 5); ?></td>
+                        <td><?php echo $rows[$i]['pro_id']; ?></td>
+                        <td><?php echo $rows[$i]['bath_number']; ?></td>
+                        <td><?php echo $rows[$i]['machine_status']; ?></td>
+                        <td><?php echo $rows[$i]['uname']; ?></td>
+                        <td><a class="btn btn-outline-success btn-sm"
+                               style="--bs-btn-padding-y: .2rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .7rem;"
+                               href="../fssbjl_down_check.php?id=<?php echo $rows[$i]['id']; ?>">关机</a>
+                        </td>
+                    </tr>
+                <?php }
                 ?>
                 </tbody>
             </table>
